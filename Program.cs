f218107c0f23f33bce9e3487212ea1f15436d8e2
riccardocannella <riccardo.cannella@studenti.unicam.it>
2017-03-24 10:42:40 +0100

    Bugfix
    Corretto il bug che impediva di compilare;
    Corretto il bug che faceva stampare numeri di giorni rimanenti negativi;
    Aggiunta gestione del caso particolare in cui un utente è nato il 29 febbraio.

--- Program.cs
+++ Program.cs
@@ -12,20 +12,45 @@ namespace UnicamCompleanno
 
             try {
                 DateTime dataDiNascita = DateTime.Parse(testoDigitatoDallUtente);
-
+                bool dataDiNascitaèBisestile = DateTime.IsLeapYear(dataDiNascita.Year);
                 //Calcoliamo gli anni dell'utente.
                 //Quando non sappiamo come si fa o quando, come in questo caso,
                 //.NET non ha un modo facile per ottenere la differenza in anni
                 //andiamo a vedere su StackOverlow se qualcuno ha già avuto la stessa necessità
                 //Il metodo Years è stato preso da lì (è definito più in basso, in questo file)
-                string anniDiDifferenza = Years(dataDiNascita, DateTime.Today);
+                int anniDiDifferenza = Years(dataDiNascita, DateTime.Today);
                 Console.WriteLine($"Oggi hai {anniDiDifferenza} anni");
 
                 //Ora calcoliamo i giorni che ci separano dal prossimo compleanno
-                DateTime dataProssimoCompleanno = new DateTime(DateTime.Today.Year, dataDiNascita.Month, dataDiNascita.Day);
+                DateTime dataProssimoCompleanno;
+                bool prossimoCompleannoQuestAnno=false;
+                bool natoIlVentinoveFebbraio=false;
+                // la differenza deve tener conto della data attuale, altrimenti restituirà un numero negativo
+                if(dataDiNascita.Day> DateTime.Today.Day && dataDiNascita.Month>DateTime.Today.Month){
+                    prossimoCompleannoQuestAnno=true;
+                }
+                // controllo anche il caso particolare in cui l'utente è nato il 29 febbraio
+                if(dataDiNascita.Month==2 && dataDiNascita.Day==29){
+                    natoIlVentinoveFebbraio=true;
+                }
+                int annoProssimoCompleanno;
+                if(prossimoCompleannoQuestAnno)
+                    annoProssimoCompleanno = DateTime.Today.Year;
+                else annoProssimoCompleanno = DateTime.Today.Year+1;
+                bool prossimoCompleannoèBisestile = DateTime.IsLeapYear(annoProssimoCompleanno);
+                // a questo punto controllo: se l'anno del prossimo compleanno non è bisestile
+                // e l'utente è nato il 29 febbraio è assegnata come data di compleanno il primo marzo
+                if((!prossimoCompleannoèBisestile) && natoIlVentinoveFebbraio)
+                dataProssimoCompleanno = new DateTime(annoProssimoCompleanno, 3, 1);
+                // altrimenti non devo preoccuparmi di nulla
+                else dataProssimoCompleanno= new DateTime(annoProssimoCompleanno, dataDiNascita.Month, dataDiNascita.Day);
+
                 TimeSpan differenzaTemporale = dataProssimoCompleanno - DateTime.Today;
-                double differenzaInGiorni = differenzaTemporale.TotalDays;
+                int differenzaInGiorni = int.Parse(differenzaTemporale.TotalDays.ToString());
+                if (differenzaInGiorni!=365)
                 Console.WriteLine($"Restano {differenzaInGiorni} giorni al tuo prossimo compleanno!");
+                else
+                Console.WriteLine($"Auguri!");
 
             } catch (Exception exc) {
                 Console.WriteLine($"Non hai digitato una data valida, il programma ora terminera'. L'errore e' stato: {exc.Message}.");
