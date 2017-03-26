23e6af754ea86aec374230f92f3cb8847ac254a5
riccardocannella <riccardo.cannella@studenti.unicam.it>
2017-03-26 12:10:30 +0200

    Bugfix
    Corretto un errore nello stabilire l'anno del prossimo compleanno

--- Program.cs
+++ Program.cs
@@ -10,7 +10,8 @@ namespace UnicamCompleanno
             Console.WriteLine("Digita la tua data di nascita: ");
             string testoDigitatoDallUtente = Console.ReadLine();
 
-            try {
+            try
+            {
                 DateTime dataDiNascita = DateTime.Parse(testoDigitatoDallUtente);
                 bool dataDiNascitaèBisestile = DateTime.IsLeapYear(dataDiNascita.Year);
                 //Calcoliamo gli anni dell'utente.
@@ -23,36 +24,39 @@ namespace UnicamCompleanno
 
                 //Ora calcoliamo i giorni che ci separano dal prossimo compleanno
                 DateTime dataProssimoCompleanno;
-                bool prossimoCompleannoQuestAnno=false;
-                bool natoIlVentinoveFebbraio=false;
+                bool prossimoCompleannoQuestAnno = true;
+                bool natoIlVentinoveFebbraio = false;
                 // la differenza deve tener conto della data attuale, altrimenti restituirà un numero negativo
-                if(dataDiNascita.Day> DateTime.Today.Day && dataDiNascita.Month>DateTime.Today.Month){
-                    prossimoCompleannoQuestAnno=true;
-                }
+                if ((dataDiNascita.Month == DateTime.Today.Month && dataDiNascita.Day <= DateTime.Today.Day) || 
+                        dataDiNascita.Month < DateTime.Today.Month)
+                    prossimoCompleannoQuestAnno = false;
                 // controllo anche il caso particolare in cui l'utente è nato il 29 febbraio
-                if(dataDiNascita.Month==2 && dataDiNascita.Day==29){
-                    natoIlVentinoveFebbraio=true;
+                if (dataDiNascita.Month == 2 && dataDiNascita.Day == 29)
+                {
+                    natoIlVentinoveFebbraio = true;
                 }
                 int annoProssimoCompleanno;
-                if(prossimoCompleannoQuestAnno)
+                if (prossimoCompleannoQuestAnno)
                     annoProssimoCompleanno = DateTime.Today.Year;
-                else annoProssimoCompleanno = DateTime.Today.Year+1;
+                else annoProssimoCompleanno = DateTime.Today.Year + 1;
                 bool prossimoCompleannoèBisestile = DateTime.IsLeapYear(annoProssimoCompleanno);
                 // a questo punto controllo: se l'anno del prossimo compleanno non è bisestile
                 // e l'utente è nato il 29 febbraio è assegnata come data di compleanno il primo marzo
-                if((!prossimoCompleannoèBisestile) && natoIlVentinoveFebbraio)
-                dataProssimoCompleanno = new DateTime(annoProssimoCompleanno, 3, 1);
+                if ((!prossimoCompleannoèBisestile) && natoIlVentinoveFebbraio)
+                    dataProssimoCompleanno = new DateTime(annoProssimoCompleanno, 3, 1);
                 // altrimenti non devo preoccuparmi di nulla
-                else dataProssimoCompleanno= new DateTime(annoProssimoCompleanno, dataDiNascita.Month, dataDiNascita.Day);
+                else dataProssimoCompleanno = new DateTime(annoProssimoCompleanno, dataDiNascita.Month, dataDiNascita.Day);
 
                 TimeSpan differenzaTemporale = dataProssimoCompleanno - DateTime.Today;
                 int differenzaInGiorni = int.Parse(differenzaTemporale.TotalDays.ToString());
-                if (differenzaInGiorni!=365)
-                Console.WriteLine($"Restano {differenzaInGiorni} giorni al tuo prossimo compleanno!");
+                if (differenzaInGiorni != 365)
+                    Console.WriteLine($"Restano {differenzaInGiorni} giorni al tuo prossimo compleanno!");
                 else
-                Console.WriteLine($"Auguri!");
+                    Console.WriteLine($"Auguri!");
 
-            } catch (Exception exc) {
+            }
+            catch (Exception exc)
+            {
                 Console.WriteLine($"Non hai digitato una data valida, il programma ora terminera'. L'errore e' stato: {exc.Message}.");
             }
             Console.ReadKey();
